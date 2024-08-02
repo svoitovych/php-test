910903790a8024686c70df73e8d2fcc68104fdff
serg <serg@surfer-172-29-6-137-hotspot.internet-for-guests.com>
2024-08-02 09:57:07 +0200

    code refactor

--- check_contact.php
+++ check_contact.php
@@ -28,23 +28,28 @@
   $_SESSION['subject'] = $subject;
   $_SESSION['message'] = $message;
 
-  if(strlen($user_name) <= 2) {
+  if (strlen($user_name) <= 2)
     $_SESSION['error_username'] = "Please enter correct name";
-    redirect();
-  } else if (strlen($from) < 5 || strpos($from, "@") === false) {
+
+  if (strlen($from) < 5 || !strpos($from, "@"))
     $_SESSION['error_email'] = "Please enter valid email address";
-    redirect();
-  } else if(strlen($subject) <= 5) {
+
+  if (strlen($subject) <= 5)
     $_SESSION['error_subject'] = "Subject should be at least 5 characters long";
-    redirect();
-  } else if(strlen($message) <= 15) {
+
+  if (strlen($message) <= 15)
     $_SESSION['error_message'] = "Message should be at least 15 characters long";
-    redirect();
-  } else {
+
+  if (!isset($_SESSION['error_username'])
+    && !isset($_SESSION['error_email'])
+    && !isset($_SESSION['error_subject'])
+    && !isset($_SESSION['error_message'])
+  ) {
     $subject = "=?UTF-8?B?".base64_encode($subject)."?=";
     $headers  = "From: $from\r\nReply-To: $from\r\nContent-type: text/plain; charset=utf-8\r\n";
-
     mail('adfasdc@gmail.com', $subject, $message, $headers);
+
     $_SESSION['success_mail'] = "Emails sent successfully";
-    redirect();
   }
+
+  redirect();
--- contacts.php
+++ contacts.php
@@ -3,10 +3,10 @@
   $title = "Contacts";
   include_once "blocks/header.php";
 
-  $username = str_contains($_SESSION['user_name'], "Undefined array key") ? '' : $_SESSION['user_name'];
-  $email = str_contains($_SESSION['email'], "Undefined array key") ? '' : $_SESSION['email'];
-  $subject = str_contains($_SESSION['subject'], "Undefined array key") ? '' : $_SESSION['subject'];
-  $message = str_contains($_SESSION['message'], "Undefined array key") ? '' : $_SESSION['message'];
+  $username = $_SESSION['user_name'];
+  $email = $_SESSION['email'];
+  $subject = $_SESSION['subject'];
+  $message = $_SESSION['message'];
 ?>
 
 <h1><?=$title?></h1>
@@ -17,16 +17,16 @@
 
 <form action="check_contact.php" method="post">
   <input type="text" name="username" value="<?=$username?>" placeholder="Enter you name" class="form-control">
-    <div class="text-danger"><?=isset($_SESSION['error_username']) ? $_SESSION['error_username'] : ''?></div>
+    <div class="text-danger"><?=$_SESSION['error_username'] ?? ''?></div>
     <br>
   <input type="email" name="email" value="<?=$email?>" placeholder="Enter you email" class="form-control">
-    <div class="text-danger"><?=isset($_SESSION['error_email']) ? $_SESSION['error_email'] : ''?></div>
+    <div class="text-danger"><?=$_SESSION['error_email'] ?? ''?></div>
     <br>
   <input type="text" name="subject" value="<?=$subject?>" placeholder="Message subject" class="form-control">
-    <div class="text-danger"><?=isset($_SESSION['error_subject']) ? $_SESSION['error_subject'] : ''?></div>
+    <div class="text-danger"><?=$_SESSION['error_subject'] ?? ''?></div>
     <br>
   <textarea name="message" placeholder="Enter a message" class="form-control"><?=$message?></textarea>
-    <div class="text-danger"><?=isset($_SESSION['error_message']) ? $_SESSION['error_message'] : ''?></div>
+    <div class="text-danger"><?=$_SESSION['error_message'] ?? ''?></div>
     <br>
   <button tupe="submit" class="btn btn-success">Send</button>
 </form>
